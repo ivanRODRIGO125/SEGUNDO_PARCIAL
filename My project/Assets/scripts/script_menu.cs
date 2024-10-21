2dc03c8c48aee80c3694d3d07c2c2b888a3efc4e
ivanRODRIGO125 <ivanrodrigolasala@gmail.com>
2024-10-21 13:50:35 -0300

    cambios
    dd

---
+++ My project/Assets/scripts/script_menu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class script_menu : MonoBehaviour
+{
+
+
+
+    public void empezar_nivel(string nivel) {
+        SceneManager.LoadScene(nivel);
+    }
+    public void Salir() { Application.Quit();
+        Debug.Log("saliste del juego");
+    }
+    
+}
